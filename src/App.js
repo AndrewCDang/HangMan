78daf22fe4e7fc5ed56696b82a7dc5f55af479db
Andrew Dang <andrewdang@Andrews-MBP-2.cust.communityfibre.co.uk>
2023-03-20 22:52:39 +0000

    Added comments to app file

--- src/App.js
+++ src/App.js
@@ -16,20 +16,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='displayContainer'>
-          {/*  */}
+          {/* Component displaying Hang Man animted SVG (animations self-produced via SVGator) */}
           <HangManSvg />
         </div>
         <div className='inputsContainer'>
+          {/* Dictionary Component, filtering out words based on length */}
           <Dictionary />
         </div>
         <div className='inputsContainer'>
+          {/* Component displays on screen keyboard built via map method */}
           <Keyboard />
+          {/* Component that toggles instruction on screen */}
           <Instructions />
+          {/* Replay button and appropriate win/defeat component displayed when 'winState' is true */}
           <Replay />
+          {/* Buttons at bottom of screen */}
           <Additional />
+          {/* Tracks the states of correct inputs, tries remaining and dispatches appropiate actions */}
           <Tracker />
+          {/* Background related to 'instructions' component, blurs background items when instructions are displayed */}
           <Background/>
         </div>
+        {/* Component dealing with victory confetti animation */}
         <WinState/>
       </header>
     </div>
